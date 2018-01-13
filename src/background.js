e5fbac397a668a37694e1b2776425bc5e43f593b
Adrian Tombu <adrian@otso.fr>
2018-01-13 18:09:15 +0100

    Disable click on empty url

--- src/background.js
+++ src/background.js
@@ -5,6 +5,8 @@ window.browser = (function () {
 let url
 
 browser.runtime.onMessage.addListener(request => (url = request.url))
-browser.browserAction.onClicked.addListener(tab =>
-  browser.tabs.create({ url: url })
-)
+browser.browserAction.onClicked.addListener(tab => {
+  if (url && tab.url.includes('amazon.')) {
+    browser.tabs.create({ url: url })
+  }
+})
