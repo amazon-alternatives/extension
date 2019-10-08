987feaf5c1c8e76fab6d4a522e83092c759843bb
Adrian Tombu <adrian@otso.fr>
2019-10-08 21:28:28 +0200

    🐛 Linting + bump version

--- src/helpers/stores.js
+++ src/helpers/stores.js
@@ -32,7 +32,7 @@ function getStores (isbn) {
       {
         title: 'buchkatalog.at',
         url: `https://www.buchkatalog.at/webapp/wcs/stores/servlet/KNVAdvancedSearchResult?storeId=270205&catalogId=4099276460822233275&langId=-3&fromAdvanceSearch=AdvanceSearch&pageType=HU&stock=&iehack=%E2%98%A0&offer=&avail=&media=All+Media&title=&keyword=&author1=&author=&actor1=&actor=&topic=&publisher1=&publisher=&movie_category=All+Categories&articleno=${isbn}&lang=&lang1=deutsch&movie_subtitle=&covertype=all&range_price=from-to&price_from=&price_to=&range_age=from-to&age_from=&age_to=&range_age1=from-to&age1_from=&age1_to=&range_age2=from-to&age2_from=&age2_to=&range_issuedate=from-to&issuedate_from=&issuedate_to=&issue1=&issue=&nott=`
-      }	 
+      }
     ],
     fr: [
       {
