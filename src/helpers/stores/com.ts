34bd699f92af7a7660839225d453e20f2b86863a
Adrian Tombu <adrian@otso.fr>
2020-02-21 12:33:47 +0100

    ✨ Add the bookshop.org source

--- src/helpers/stores/com.ts
+++ src/helpers/stores/com.ts
@@ -7,6 +7,11 @@ export const stores: Store[] = [
     categories: [Category.ENGLISH_BOOKS, Category.STRIPBOOKS, Category.BOOKS],
   },
   {
+    title: 'bookshop.org',
+    url: 'https://bookshop.org/books?keywords=%1$s',
+    categories: [Category.ENGLISH_BOOKS, Category.STRIPBOOKS, Category.BOOKS],
+  },
+  {
     title: 'kobo.com',
     url: 'https://www.kobo.com/us/en/search?query=%1$s',
     categories: [Category.DIGITAL_TEXT],
